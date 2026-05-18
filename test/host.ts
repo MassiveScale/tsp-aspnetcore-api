813c3f838d27482039143ffd031757700cad8e87
Marc LaFleur <marc.lafleur@marketonce.com>
2026-05-18 12:57:53 -0400

    Refactor code formatting and update configuration files
    - Improved code formatting in test/host.ts for better readability.
    - Updated tsconfig.json to streamline library inclusion and removed unnecessary line breaks.
    - Added .prettierignore to exclude specific directories from formatting.

--- test/host.ts
+++ test/host.ts
@@ -3,7 +3,11 @@ import { expectDiagnosticEmpty } from "@typespec/compiler/testing";
 import { createTester } from "@typespec/compiler/testing";
 
 const baseTester = createTester(resolvePath(import.meta.dirname, "../.."), {
-  libraries: ["@massivescale/tsp-aspnetcore-api", "@typespec/http", "@typespec/versioning"],
+  libraries: [
+    "@massivescale/tsp-aspnetcore-api",
+    "@typespec/http",
+    "@typespec/versioning",
+  ],
 });
 
 export const Tester = baseTester.emit("@massivescale/tsp-aspnetcore-api");
@@ -12,7 +16,9 @@ export async function emitWithDiagnostics(
   code: string,
   options?: Record<string, unknown>,
 ): Promise<[Record<string, string>, readonly Diagnostic[]]> {
-  const tester = options ? baseTester.emit("@massivescale/tsp-aspnetcore-api", options) : Tester;
+  const tester = options
+    ? baseTester.emit("@massivescale/tsp-aspnetcore-api", options)
+    : Tester;
   const [{ outputs }, diagnostics] = await tester.compileAndDiagnose(code);
   return [outputs, diagnostics];
 }
