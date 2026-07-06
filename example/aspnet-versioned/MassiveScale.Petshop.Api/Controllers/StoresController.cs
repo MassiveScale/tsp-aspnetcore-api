06d8ddd9536b5a06dac0d8bd7c96f9d57044af91
Marc LaFleur <marc.lafleur@marketonce.com>
2026-07-06 10:15:33 -0400

    Add asp.net example

---
+++ example/aspnet-versioned/MassiveScale.Petshop.Api/Controllers/StoresController.cs
@@ -0,0 +1,34 @@
+﻿using MarketOnce.Community.Campaign.Api.Controllers;
+using MarketOnce.Community.Campaign.Api.Helpers;
+using MarketOnce.Community.Campaign.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MassiveScale.Petshop.Api.Controllers;
+
+public class StoresController : StoresControllerBase
+{
+    public override Task<IActionResult> Create([FromBody] Store body, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<IActionResult> Delete([FromRoute] string id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<IActionResult> List(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<IActionResult> Read([FromRoute] string id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<IActionResult> Update([FromRoute] string id, [FromBody] MergePatch<Store> body, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ example/aspnet-versioned/MassiveScale.Petshop.Api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.MapOpenApi();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
+app.UseHttpsRedirection();
+app.Run();
+
